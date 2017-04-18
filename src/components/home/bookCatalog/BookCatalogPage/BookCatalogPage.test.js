cb7e0eda3caa17a84af3ea8b36f2f01d6ecd182c
Philippe Mimms <philippe.mimms@gmail.com>
2017-04-18 00:08:56 -0400

    Adds ignore-styles as a dependency Reintroduces failing unit test suites

--- src/components/home/bookCatalog/BookCatalogPage/BookCatalogPage.test.js
+++ src/components/home/bookCatalog/BookCatalogPage/BookCatalogPage.test.js
@@ -1,4 +1,3 @@
-/*
 import { BookCatalogPage } from '../BookCatalogPage';
 //import expect from 'expect';
 import { mount } from 'enzyme';
@@ -6,12 +5,16 @@ import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import React from 'react';
 import injectTapEventPlugin from 'react-tap-event-plugin';
 
-/* Needed for Material-UI click events
+/* Needed for Material-UI click events */
 injectTapEventPlugin();
 
-function setup({ books = [] } = {}) {
+function setup({ bookDeadline, books, deleteBook, saveBook, saveBookDeadline } = {}) {
     const props = {
-        books
+        bookDeadline,
+        books: books || [],
+        deleteBook: deleteBook || Function.prototype,
+        saveBook: saveBook || Function.prototype,
+        saveBookDeadline: saveBookDeadline || Function.prototype
     };
 
     return mount(<MuiThemeProvider><BookCatalogPage {...props} /></MuiThemeProvider>);
@@ -22,4 +25,3 @@ describe('Book Catalog Page', () => {
         setup();
     });
 });
-*/
--- src/components/home/bookCatalog/BookCatalogPage/BookList/BookListRow/BookListRow.test.js
+++ src/components/home/bookCatalog/BookCatalogPage/BookList/BookListRow/BookListRow.test.js
@@ -1,12 +1,14 @@
-/*
 import BookListRow from '../BookListRow';
 import expect from 'expect';
 import { shallow } from 'enzyme';
 import React from 'react';
+import { TableRow } from 'material-ui/Table';
 
-function setup(book) {
+function setup({ book = {}, deleteBook, saveBook }) {
     const props = {
-        book
+        book,
+        deleteBook: deleteBook || Function.prototype,
+        saveBook: saveBook || Function.prototype
     };
 
     return shallow(<BookListRow {...props} />);
@@ -14,9 +16,7 @@ function setup(book) {
 
 describe('Book List Row', () => {
     it('Renders <div> with Book Title as innerHTML', () => {
-        const wrapper = setup({title: 'Batman'});
-        expect(wrapper.find('div').length).toBe(1);
-        expect(wrapper.find('div').text()).toEqual('Batman');
+        const wrapper = setup({});
+        expect(wrapper.find(TableRow).length).toBe(1);
     });
 });
-*/
