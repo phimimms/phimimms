40368899e04f6d6debf9bf6949f1e52f44ee93a6
Philippe Mimms <philippe.mimms@gmail.com>
2019-05-14 13:48:31 -0400

    Configures ESLint to support Svelte files;

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,19 +1,21 @@
 module.exports = {
   "env": {
     "browser": true,
+    "es6": true,
     "jest": true,
   },
   "extends": [ "plugin:@typescript-eslint/recommended" ],
   "parser": "@typescript-eslint/parser",
   "parserOptions": {
-    "ecmaVersion": 6,
-    "sourceType": "module",
     "ecmaFeatures": {
       "modules": true,
     },
+    "ecmaVersion": 2019,
+    "sourceType": "module",
   },
   "plugins": [
     "@typescript-eslint",
+    "svelte3",
   ],
   "rules": {
     "@typescript-eslint/indent": [ "error", 2 ],
